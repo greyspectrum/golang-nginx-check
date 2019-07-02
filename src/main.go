e9c073414d1f6b8a28e5723e558869d541d54cc7
greyspectrum <austin_smith@harvard.edu>
2019-07-02 09:15:13 -0400

    initial commit

---
+++ src/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "os/exec"
+    "fmt"
+    "os"
+)
+
+func main() {
+    cmd := exec.Command("systemctl", "check", "nginx")
+    out, err := cmd.CombinedOutput()
+    if err != nil {
+        fmt.Println("Cannot find process")
+        os.Exit(1)
+    }
+    fmt.Printf("Status is: %s", string(out))
+    fmt.Println("Starting Role")
