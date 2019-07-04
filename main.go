c2fd70215cb447950de296fdf36cdcfc61a8b8bc
greyspectrum <austin_smith@harvard.edu>
2019-07-04 17:28:44 -0400

    add infinite loop

--- main.go
+++ main.go
@@ -1,19 +1,20 @@
 package main
 
 import (
-    "os/exec"
-    "fmt"
-    "os"
+	"fmt"
+	"os/exec"
 )
 
 func main() {
-    cmdStatus := exec.Command("systemctl", "check", "nginx")
-    cmdRestart := exec.Command("systemctl", "restart", "nginx")
-    out, err := cmdStatus.CombinedOutput()
-    if err != nil {
-        fmt.Println("Cannot find process")
-	cmdRestart.Run()
-        os.Exit(1)
-    }
-    fmt.Printf("Nginx is: %s", string(out))
+	for {
+		cmdStatus := exec.Command("systemctl", "check", "nginx")
+		cmdRestart := exec.Command("systemctl", "restart", "nginx")
+		out, err := cmdStatus.CombinedOutput()
+		if err != nil {
+			fmt.Println("Cannot find process")
+			cmdRestart.Run()
+		} else {
+			fmt.Printf("Nginx is: %s", string(out))
+		}
+	}
 }
